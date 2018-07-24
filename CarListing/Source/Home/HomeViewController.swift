521a348e60410fabef59409dbdf5fb81af2d93c4
Mehdi <mmbadjian@gmail.com>
2018-07-24 15:46:44 +0800

    Added Table to home view and list car cell

--- CarListing/Source/Home/HomeViewController.swift
+++ CarListing/Source/Home/HomeViewController.swift
@@ -9,17 +9,47 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+  @IBOutlet weak var tableView: UITableView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
+    self.title = "Home"
+    self.view.backgroundColor = .white
+    
+    self.tableView.delegate = self
+    self.tableView.dataSource = self
+    self.tableView.register(ListingCarTableViewCell.self, forCellReuseIdentifier: ListingCarTableViewCell.identifier)
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
-
 
 }
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+
+  
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
+  }
+  
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 110
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ListingCarTableViewCell", for: indexPath) as! ListingCarTableViewCell
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+  }
+  
+}
+
+
+
 
---
+++ CarListing/Source/Home/ListingCarTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ListingCarCellTableViewCell.swift
+//  CarListing
+//
+//  Created by Mehdi on 24/07/2018.
+//  Copyright © 2018 Mehdi. All rights reserved.
+//
+
+import UIKit
+
+class ListingCarTableViewCell: UITableViewCell {
+  static var identifier: String {
+    return String(describing: self)
+  }
+}
